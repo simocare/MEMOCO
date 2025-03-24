903d6930d043cc9b81ea51a50b455f882f7c7c08
simocare <simone.caregnato@studenti.unipd.it>
2025-03-24 22:49:38 +0100

    fix board generation: no repetition in random choice of holes

--- generate_board.cpp
+++ generate_board.cpp
@@ -3,15 +3,19 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <set>
 
 void generateBoard(int size, int num_holes, const std::string& filename) {
     std::vector<std::vector<int>> board(size, std::vector<int>(size, 0));
+    std::set<std::pair<int, int>> used_positions;
     srand(time(nullptr));
 
-    for (int i = 0; i < num_holes; ++i) {
+    while (used_positions.size() < static_cast<size_t>(num_holes)) {
         int x = rand() % size;
         int y = rand() % size;
-        board[y][x] = 1; // Mark hole position
+        if (used_positions.insert({x, y}).second) {
+            board[y][x] = 1;
+        }
     }
 
     std::ofstream outFile(filename);
@@ -33,14 +37,16 @@ void generateBoard(int size, int num_holes, const std::string& filename) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <size> <num_holes>" << std::endl;
+    if (argc < 3 || argc > 4) { // Allow 3 or 4 arguments
+        std::cerr << "Usage: " << argv[0] << " <size> <num_holes> [output_filename]" << std::endl;
         return 1;
     }
     
     int size = std::stoi(argv[1]);
     int num_holes = std::stoi(argv[2]);
-    
-    generateBoard(size, num_holes, "board.dat");
+
+    std::string filename = (argc == 4) ? argv[3] : "board.dat"; // Use provided filename or default
+
+    generateBoard(size, num_holes, filename);
     return 0;
 }
